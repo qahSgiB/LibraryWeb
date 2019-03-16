dd3da195e03e06bfcecacaba4ffcc36649389406
qahSgiB <tomas.sumsala@gmail.com>
2019-03-16 18:19:18 +0100

    Big update

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ from routes import htmlRoutes, errorRoutes
 
 database = MongoClient('localhost', 27017)['books']
 
+def findBooks(searchBook):
+    def isResult(book, searchBook):
+        return True
+        # return searchBook == book['title']
+
+    books = database['books'].find({})
+    booksFound = []
+
+    for book in books:
+        if isResult(book, searchBook):
+            booksFound.append(book)
+
+    return booksFound
+
 
 
 def main():
--- pyPage/Template.py
+++ pyPage/Template.py
@@ -1,2 +1,26 @@
+class Template():
+    def __init__(self, template=None):
+        self.template = template
+
+    def load(templateName):
+        template = None
+
+        with open(templatePath(templateName), 'r') as templateFile:
+            template = templateFile.read()
+
+        return Template(template)
+
+    def format(self, context):
+        return self.template.format(**context)
+
+    def formatMultiple(self, contexts):
+        html = ''
+        for context in contexts:
+            html += self.format(context)
+
+        return html
+
+
+
 def templatePath(template):
     return f'page/templates/{template}'
